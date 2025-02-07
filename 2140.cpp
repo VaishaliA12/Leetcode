4816969284582bd6b9010eb37d5132f5674c7478
VaishaliA12 <avaishali96@gmail.com>
2025-02-07 19:56:42 +0530

    done

---
+++ 2140.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n=questions.size();
+        vector<long long> dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int point=questions[i][0];
+            int jump=questions[i][1];
+            int nextQues=min(n,i+jump+1);
+            dp[i]=max(dp[i+1],point+dp[nextQues]);
+        }
+        return dp[0];
+    }
+};
