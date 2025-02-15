c71ff3962f837be151a0bdaca26ad41f3d2b8ec9
VaishaliA12 <avaishali96@gmail.com>
2025-02-15 13:55:51 +0530

    done

---
+++ 1911.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        long long maxAlternatingSum(vector<int>& nums) {
+            int n=nums.size();
+            vector<vector<long long>> dp(n+1,vector<long long> (2,0));
+            for(int i=1;i<n+1;i++){
+                dp[i][0]=max(dp[i-1][1]-nums[i-1],dp[i-1][0]);
+                dp[i][1]=max(dp[i-1][0]+nums[i-1],dp[i-1][1]);
+            }
+            return max(dp[n][1],dp[n][1]);
+        }
+    };
